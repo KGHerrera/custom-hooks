8064c0174df9a1fcb4e1f1e4ddf77a9ca3c5a39b
Enrique Herrera <bycrashdash@gmail.com>
2022-07-19 19:49:02 -0500

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import HooksPersonalizados from './components/HooksPersonalizados';
+import Referencias from './components/Referencias';
+
+function App() {
+  return (
+    <div className="App">
+      <HooksPersonalizados />
+      <Referencias />
+    </div>
+  );
+}
+
+export default App;
